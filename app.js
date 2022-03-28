f7d415a003083ebac6cb8b3c6c877d85354aaeb5
Thayssa Apolinário <thayssaoliveira55@gmail.com>
2022-03-28 15:28:06 -0300

    ajustes

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const express = require('express');
+const res = require('express/lib/response');
+const port = 3030
+const app = express()
 
-const nodemon = require('nodemon');
+app.get('/', (req, res) => {
+    res.send('Página Inicial')
+})
+
+app.get('/sobre', (req, res) => {
+    res.send('Sobre o nosso projeto')
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Fale conosco')
+})
+
+app.listen(port, () => {
+    console.log('O servidor está funcionando na porta ' + port)
+})
